e4101cd40483a8c1f0f84ffbff9a8ba35fe1f69d
spinachh <kivislime@HypocritPc.>
2025-02-25 11:14:34 +0700

    Added CORSFilter

---
+++ src/main/java/org/kivislime/currencyexchange/util/CORSFilter.java
@@ -0,0 +1,18 @@
+package org.kivislime.currencyexchange.util;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class CORSFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
