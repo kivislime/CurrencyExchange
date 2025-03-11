bc47ee14c50235645e31fb968ed154c4e0715200
spinachh <kivislime@HypocritPc.>
2025-03-11 18:00:28 +0700

    create backend folder and move src files there, add docker compose & frontend

--- backend/src/main/java/org/kivislime/currencyexchange/DatabaseConnectionManager.java
+++ backend/src/main/java/org/kivislime/currencyexchange/DatabaseConnectionManager.java
@@ -5,9 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnectionManager {
-    private static final String URL = "jdbc:postgresql://DB_currency_exchange:5432/postgres";
-    private static final String USER = "myuser";
-    private static final String PASS = "mysecretpassword";
+    private static final String URL = System.getenv("DB_URL") != null
+            ? System.getenv("DB_URL")
+            : "jdbc:postgresql://DB_currency_exchange:5432/postgres";
+    private static final String USER = System.getenv("DB_USER") != null
+            ? System.getenv("DB_USER")
+            : "myuser";
+    private static final String PASS = System.getenv("DB_PASS") != null
+            ? System.getenv("DB_PASS")
+            : "mysecretpassword";
 
     static {
         try {
@@ -20,5 +26,4 @@ public class DatabaseConnectionManager {
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
     }
-
 }
---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+cd backend || exit 1
+mvn clean package -DskipTests
+cd ..
+
+docker-compose up -d
+
+if ! docker ps | grep -q "tomcat_app"; then
+  echo "error: tomcat_app container is not running!"
+  exit 1
+fi
+
+docker cp backend/target/CurrencyExchange-1.0-SNAPSHOT.war tomcat_app:/usr/local/tomcat/webapps/currency-exchange.war
+
+docker restart tomcat_app
+
+echo "Deploy success!"
